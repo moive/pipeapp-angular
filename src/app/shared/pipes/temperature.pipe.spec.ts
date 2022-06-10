fcece1eba9ec21a04aeace54c4743d17ed59be94
Moises Velasquez <mvelasquez@pictoric.com>
2022-06-09 22:54:23 -0500

    test temperature.pipe.spec

--- src/app/shared/pipes/temperature.pipe.spec.ts
+++ src/app/shared/pipes/temperature.pipe.spec.ts
@@ -1,8 +1,21 @@
+import { TemperatureValues } from '../enums/temperature.enum';
 import { TemperaturePipe } from './temperature.pipe';
 
 describe('TemperaturePipe', () => {
   it('create an instance', () => {
     const pipe = new TemperaturePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('Should return C degrees if I send a value in C', () => {
+    const pipe = new TemperaturePipe();
+
+    expect(pipe.transform(10, TemperatureValues.C)).toEqual('10°C');
+  });
+
+  it('Should return F degrees if I send a value F', () => {
+    const pipe = new TemperaturePipe();
+
+    expect(pipe.transform(12, TemperatureValues.F)).toEqual('53.6°F');
   });
 });
